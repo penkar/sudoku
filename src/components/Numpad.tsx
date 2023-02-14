5f2dd79288b28bd9faaadce519a1ab7c1dd2a0b9
JSP <jspenkar@gmail.com>
2023-02-13 23:30:56 -0600

    Numpad nums that are complete should be marked as such.

--- src/components/Numpad.tsx
+++ src/components/Numpad.tsx
@@ -1,9 +1,16 @@
 import { Num } from "./Num";
 import { useBoard } from "../contexts/useBoard";
 
-export const Numpad = () => {
-  const { actions, currentNumber } = useBoard();
+interface BoardFieldValue {
+  fieldValue: number;
+}
 
+export const Numpad = () => {
+  const { actions, board, currentNumber } = useBoard();
+  const completedArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  board.forEach(({ fieldValue }) => {
+    completedArray[fieldValue] += 1;
+  });
   const props = {
     clickEvent: actions?.selectNumber,
     currentNumber,
@@ -11,15 +18,15 @@ export const Numpad = () => {
 
   return (
     <div className="numpad-component">
-      <Num complete={false} value={1} {...props} />
-      <Num complete={false} value={2} {...props} />
-      <Num complete={false} value={3} {...props} />
-      <Num complete={false} value={4} {...props} />
-      <Num complete={false} value={5} {...props} />
-      <Num complete={false} value={6} {...props} />
-      <Num complete={false} value={7} {...props} />
-      <Num complete={false} value={8} {...props} />
-      <Num complete={false} value={9} {...props} />
+      <Num complete={completedArray[1] === 9} value={1} {...props} />
+      <Num complete={completedArray[2] === 9} value={2} {...props} />
+      <Num complete={completedArray[3] === 9} value={3} {...props} />
+      <Num complete={completedArray[4] === 9} value={4} {...props} />
+      <Num complete={completedArray[5] === 9} value={5} {...props} />
+      <Num complete={completedArray[6] === 9} value={6} {...props} />
+      <Num complete={completedArray[7] === 9} value={7} {...props} />
+      <Num complete={completedArray[8] === 9} value={8} {...props} />
+      <Num complete={completedArray[9] === 9} value={9} {...props} />
     </div>
   );
 };
